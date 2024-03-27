79d3a87a85a2eafb9900fc18d66f5479a316b8aa
Manioco <prausartudo@gmail.com>
2024-03-27 14:38:43 -0300

    Link import fix

--- client/src/pages/players.jsx
+++ client/src/pages/players.jsx
@@ -1,6 +1,7 @@
-import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { Link } from 'react-router-dom';
+import React from "react";
 import axios from "axios";
 
 const Players = () => {
